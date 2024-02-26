1c5e9f3bdda2bfb953bfee0ea68dc7fd3162a809
Yamin Hüseynov <146395517+yaminhuseynov@users.noreply.github.com>
2024-02-26 23:59:43 +0100

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,8 @@
+let Name = "Mustermann";
+let Familienname = "Mustermann";
+let alt = 30;
+let Stadt = "Stuttgart";
+
+let msg = `Mein name ist ${Name} und Familienname ist ${Familienname}. Ich bin ${alt} alt. Ich wurde in ${Stadt} geboren.`;
+
+console.log(msg);
