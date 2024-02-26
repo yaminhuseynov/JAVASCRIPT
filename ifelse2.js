2c7b61c544661aaebd7c5367cba4c917db8cef98
Yamin Hüseynov <146395517+yaminhuseynov@users.noreply.github.com>
2024-02-26 22:50:47 +0100

    Add files via upload
    Einen Beispiel für Führerschein in Javascript um ob mann bekommen kann oder nicht.

---
+++ ifelse2.js
@@ -0,0 +1,12 @@
+let Alt = 22;
+let Abschluss = "Universität"; 
+
+if (Alt >=20 && mezuniyyet =="Universität"){
+
+    console.log("Gültig, kann Führerschein bekommen")
+}
+    
+
+else {
+    console.log("Kann nicht bekommen");
+}
