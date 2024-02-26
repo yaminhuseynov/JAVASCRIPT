3539f5a61a3c5a224ee4fac5d0f6ef655be61c1c
Yamin Hüseynov <146395517+yaminhuseynov@users.noreply.github.com>
2024-02-26 22:44:51 +0100

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+let username = "Yamin";
+let password = "1234";
+
+if (username == "Yamin") {
+  if (password == "1234") console.log("Willkommen!");
+  else {
+    console.log("Passwort ist falsch!");
+  }
+} else {
+  console.log("Kontoname oder Passwort ist falsch!");
+}
