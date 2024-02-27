07e8ee373608f8ce31ca698002114411b4f61ce0
Yamin Hüseynov <146395517+yaminhuseynov@users.noreply.github.com>
2024-02-27 21:44:12 +0100

    Update ifelse.js

--- ifelse.js
+++ ifelse.js
@@ -2,11 +2,13 @@ let username = "Yamin";
 let password = "1234";
 
 if (username == "Yamin") {
-  if (password == "1234") 
+  if (password == "1234"){
     console.log("Willkommen!");
+  }
   else {
     console.log("Passwort ist falsch!");
   }
-} else {
+} 
+else {
   console.log("Kontoname oder Passwort ist falsch!");
 }
