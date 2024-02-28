ed96d01c5d95013ed5089bb11a5c6ce25618157a
Yamin Hüseynov <146395517+yaminhuseynov@users.noreply.github.com>
2024-02-28 22:36:24 +0100

    Add files via upload

---
+++ ifelse3.js
@@ -0,0 +1,15 @@
+let mobile = "Samsung";
+let year = 2016;
+let price = 599;
+let money = 600;
+
+
+  if (money > price && year > 2015){
+    console.log("You can buy a phone");
+  }
+  else {
+    console.log("You have not enough money")
+  }
+  if (money > price && year < 2015){
+    console.log("Year is too old");
+  }
