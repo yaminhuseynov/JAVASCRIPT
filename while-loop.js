31b808ea460580f0cee8ac435db5e4c67354a132
Yamin Hüseynov <146395517+yaminhuseynov@users.noreply.github.com>
2024-02-29 19:09:02 +0100

    Add files via upload

---
+++ while-loop.js
@@ -0,0 +1,18 @@
+let i = 5;
+  while (i < 10) {
+    console.log(i);
+
+    if (i==5) {
+      console.log("it stops here because i is equal to 5");
+      break;
+    }
+
+    else {
+      continue;
+    }
+      
+  }
+
+
+// wenn i ist 5, stoppt die Terminal. Außer 5 dauert Terminal für immer.
+//Code by Yamin Huseynov. 
